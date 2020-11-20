5c3fb991ae275caa0828882caebbb032cf03b341
Leydi Velazco <leydi.velazco@unmsm.edu.pe>
2020-11-19 20:58:05 -0500

    Kusawasi #7: Restablecer plataforma

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -9,7 +9,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Resetabelcer Contraseña') }}</div>
+                <div class="card-header">{{ __('Restablecer Contraseña') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
