1fc8d05fd6f39bfeb312fc4120f5d79b744790b4
CarlosR <carlos.rodriguez48@unmsm.edu.pe>
2020-11-19 22:44:50 -0500

    Kusawasi #12: Agregando marcas

---
+++ base de datos/kusawasi_colors.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: kusawasi
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `colors`
+--
+
+LOCK TABLES `colors` WRITE;
+/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
+INSERT INTO `colors` VALUES (1,'negro','#000000'),(2,'plata','#C0C0C0'),(3,'gris','#808080'),(4,'blanco','#FFFFFF'),(5,'granate','#800000'),(6,'rojo','#FF0000'),(7,'purpura','#800080'),(8,'fucsia','#FF00FF'),(9,'verde','#008000'),(10,'lima','#00FF00'),(11,'olivo','#808000'),(12,'amarillo','#FFFF00'),(13,'azul marino','#000080'),(14,'azul','#0000FF'),(15,'azul verdoso','#008080'),(16,'agua','#00FFFF'),(17,'azul oscuro','#00008B'),(18,'azul medio','#0000CD'),(19,'verde oscuro','#006400'),(20,'cian oscuro','#008B8B'),(21,'celeste profundo','#00BFFF'),(22,'turquesa oscuro','#00CED1'),(23,'verde primavera medio','#00FA9A'),(24,'verde primavera','#00FF7F'),(25,'cian','#00FFFF'),(26,'azul medianoche','#191970'),(27,'verde marino claro','#20B2AA'),(28,'verde bosque','#228B22'),(29,'verde marino','#2E8B57'),(30,'gris oscuro','#2F4F4F'),(31,'verde lima','#32CD32'),(32,'verde marino medio','#3CB371'),(33,'turquesa','#40E0D0'),(34,'azul real','#4169E1'),(35,'azul acero','#4682B4'),(36,'turquesa medio','#48D1CC'),(37,'indigo','#4B0082'),(38,'verde olivo medio','#556B2F'),(39,'azul cadete','#5F9EA0'),(40,'agua marina medio','#66CDAA'),(41,'gris tenue','#696969'),(42,'verde cesped','#7CFC00'),(43,'agua marina','#7FFFD4'),(44,'celeste','#87CEEB'),(45,'celeste claro','#87CEFA'),(46,'azul violeta','#8A2BE2'),(47,'rojo oscuro','#8B0000'),(48,'magenta oscuro','#8B008B'),(49,'verde claro','#90EE90'),(50,'purpura medio','#9370DB'),(51,'violeta oscuro','#9400D3'),(52,'verde palido','#98FB98'),(53,'orquidea oscuro','#9932CC'),(54,'amarillo verdoso','#9ACD32'),(55,'marron','#A52A2A'),(56,'chocolate','#D2691E'),(57,'orquidea','#DA70D6'),(58,'carmesi','#DC143C'),(59,'ciruela','#DDA0DD'),(60,'lavanda','#E6E6FA'),(61,'violeta','#EE82EE'),(62,'coral claro','#F08080'),(63,'azure','#F0FFFF'),(64,'beige','#F5F5DC'),(65,'blanco humo','#F5F5F5'),(66,'salmon','#FA8072'),(67,'magenta','#FF00FF'),(68,'anaranjado','#FF4500'),(69,'tomate','#FF6347'),(70,'coral','#FF7F50'),(71,'naranja oscuro','#FF8C00'),(72,'naranja','#FFA500'),(73,'rosa','#FFC0CB'),(74,'dorado','#FFD700'),(75,'mocasin','#FFE4B5'),(76,'nieve','#FFFAFA'),(77,'amarillo claro','#FFFFE0');
+/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-18 14:17:57
